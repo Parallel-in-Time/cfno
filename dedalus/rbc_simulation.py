c4a220f1d5b9a74bcbb606d37ec66e9b052183d4
Thibaut Lunet <thibaut.lunet@tuhh.de>
2024-08-30 19:33:17 +0200

    TL: code update for dedalus, first parallel prototype (MPI+multiprocessing)

--- dedalus/rbc_simulation.py
+++ dedalus/rbc_simulation.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import numpy as np
 from datetime import datetime
@@ -30,6 +31,11 @@ def runSim(dirName, Rayleigh=1e6, resFactor=1, baseDt=1e-2/2, seed=999,
     seed : int, optional
         Seed for the random noise in the initial solution. The default is 999.
     """
+    if os.path.isfile(f"{dirName}/01_finalized.txt"):
+        if MPI_RANK == 0:
+            print(" -- simulation already finalized, skipping !")
+        return
+
     def log(msg):
         if MPI_RANK == 0:
             with open(f"{dirName}/simu.log", "a") as f:
@@ -105,18 +111,23 @@ def runSim(dirName, Rayleigh=1e6, resFactor=1, baseDt=1e-2/2, seed=999,
         b['g'] *= z * (Lz - z) # Damp noise at walls
         b['g'] += Lz - z # Add linear background
     else:
-        b['g'] = initFields["buoyancy"][-1]
-        p['g'] = initFields["pressure"][-1]
-        u['g'] = initFields["velocity"][-1]
-        tau_p['g'] = initFields["tau_p"][-1]
-        tau_b1['g'] = initFields["tau_b1"][-1]
-        tau_b2['g'] = initFields["tau_b2"][-1]
-        tau_u1['g'] = initFields["tau_u1"][-1]
-        tau_u2['g'] = initFields["tau_u2"][-1]
+        fields = [
+            (b, "buoyancy"), (p, "pressure"), (u, "velocity"),
+            *[(f, f.name) for f in [tau_p, tau_b1, tau_b2, tau_u1, tau_u2]]
+            ]
+        for field, name in fields:
+            localSlices = (slice(None),) * len(field.tensorsig) \
+                + dist.grid_layout.slices(field.domain, field.scales)
+            field['g'] = initFields[name][-1][localSlices]
 
     # Analysis
+    iterWrite = dtWrite/timestep
+    if int(iterWrite) != round(iterWrite, ndigits=3):
+        raise ValueError(
+            f"dtWrite ({dtWrite}) is not divisible by timestep ({timestep}) : {iterWrite}")
+    iterWrite = int(iterWrite)
     snapshots = solver.evaluator.add_file_handler(
-        dirName, sim_dt=dtWrite, max_writes=1600)
+        dirName, iter=iterWrite, max_writes=1600)
     snapshots.add_task(u, name='velocity')
     snapshots.add_task(b, name='buoyancy')
     snapshots.add_task(p, name='pressure')
@@ -130,17 +141,24 @@ def runSim(dirName, Rayleigh=1e6, resFactor=1, baseDt=1e-2/2, seed=999,
         snapshots.add_task(-d3.div(d3.skew(u)), name='vorticity')
 
     # Main loop
+    nSteps = tEnd/timestep
+    if int(nSteps) != round(nSteps, ndigits=3):
+        raise ValueError(
+            f"tEnd ({tEnd}) is not divisible by timestep ({timestep})")
+    nSteps = int(nSteps)
     try:
         log('Starting main loop')
-        while solver.proceed:
+        for _ in range(nSteps+1): # need to do one more step to write last solution ...
             solver.step(timestep)
-            # if (solver.iteration-1) % 100 == 0:
-            log(f'Iteration={solver.iteration}, Time={solver.sim_time}, dt={timestep}')
+            if (solver.iteration-1) % 100 == 0:
+                log(f'Iteration={solver.iteration}, Time={solver.sim_time}, dt={timestep}')
     except:
         log('Exception raised, triggering end of main loop.')
         raise
     finally:
         solver.log_stats()
+        with open(f"{dirName}/01_finalized.txt", "w") as f:
+            f.write("Done !")
 
 
 if __name__ == '__main__':
--- dedalus/sdcRuns.py
+++ dedalus/sdcRuns.py
@@ -6,11 +6,14 @@ Base script to run tests with SDC
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from mpi4py import MPI
 
 from rbc_simulation import runSim
 from data_processing import OutputFiles
 from pySDC.playgrounds.dedalus.sdc import SpectralDeferredCorrectionIMEX
 
+
+comm = MPI.COMM_WORLD
 nX, nZ = 256, 64
 resFactor = 1
 baseDir = f"run_sdc_M{resFactor}"
@@ -19,59 +22,57 @@ Rayleigh = 1.5e7
 
 # Initial run
 dirName = f"{baseDir}/run_init"
-os.makedirs(dirName, exist_ok=True)
 dt = 1e-2/2
-if False:
-    print(f" -- running initial simulation with dt={dt:1.1e} in {dirName}")
-    runSim(dirName, Rayleigh, resFactor, baseDt=dt, useSDC=False, tEnd=100,
-           dtWrite=1, writeFull=True)
+os.makedirs(dirName, exist_ok=True)
+print(f" -- running initial simulation with dt={dt:1.1e} in {dirName}")
+runSim(dirName, Rayleigh, resFactor, baseDt=dt, useSDC=False, tEnd=100,
+       dtWrite=1, writeFull=True)
 # -- extract initial field
 initFields = OutputFiles(dirName).file(0)['tasks']
 
 
 tEnd = 1
 dtRef = 1e-6
-dtBase = 1e-2/2
-dtSizes = [dtBase/2**i for i in range(4)]
 var = "velocity"
+dtBase = 0.05
+dtSizes = [dtBase/2**i for i in range(5)]
 
 
 # Reference solution
 dirName = f"{baseDir}/run_ref"
 os.makedirs(dirName, exist_ok=True)
-if False:
-    print(f" -- running reference simulation with dt={dt:1.1e} in {dirName}")
-    runSim(dirName, Rayleigh, resFactor, baseDt=dtRef, useSDC=False,
-           tEnd=tEnd, dtWrite=tEnd, initFields=initFields)
-refFields = OutputFiles(dirName).file(0)['tasks']
+print(f" -- running reference simulation with dt={dt:1.1e} in {dirName}")
+runSim(dirName, Rayleigh, resFactor, baseDt=dtRef, useSDC=False,
+       tEnd=tEnd, dtWrite=tEnd/10, initFields=initFields)
+refFiles = OutputFiles(dirName)
+refFields = refFiles.file(0)['tasks']
 
 
 # SDC runs
 SpectralDeferredCorrectionIMEX.setParameters(
-    nSweeps=4,
+    nSweeps=3,
     nNodes=4,
-    implSweep="MIN-SR-FLEX",
+    implSweep="MIN-SR-S",
     explSweep="PIC")
 
 plt.figure("convergence")
 
-if False:
-    errors = []
-    for i, dt in enumerate(dtSizes):
-        dirName = f"{baseDir}/run_sdc_dt{dt:1.1e}"
-        os.makedirs(dirName, exist_ok=True)
-        if False:
-            print(f" -- running SDC simulation with dt={dt:1.1e} in {dirName}")
-            runSim(dirName, Rayleigh, resFactor, baseDt=dt, useSDC=True,
-                   tEnd=tEnd, dtWrite=tEnd, initFields=initFields)
-        numFields = OutputFiles(dirName).file(0)['tasks']
-        diff = numFields[var][-1] - refFields[var][-1]
-        err = np.linalg.norm(
-            diff, ord=np.inf, axis=(1,2) if var == "velocity" else None)
-        if var == "velocity":
-            err = np.mean(err)
-        errors.append(err)
-    plt.loglog(dtSizes, errors, label="SDC")
+
+errors = []
+for i, dt in enumerate(dtSizes):
+    dirName = f"{baseDir}/run_sdc_dt{dt:1.1e}"
+    os.makedirs(dirName, exist_ok=True)
+    print(f" -- running SDC simulation with dt={dt:1.1e} in {dirName}")
+    runSim(dirName, Rayleigh, resFactor, baseDt=dt, useSDC=True,
+           tEnd=tEnd, dtWrite=tEnd, initFields=initFields)
+    numFields = OutputFiles(dirName).file(0)['tasks']
+    diff = numFields[var][-1] - refFields[var][-1]
+    err = np.linalg.norm(
+        diff, ord=np.inf, axis=(1,2) if var == "velocity" else None)
+    if var == "velocity":
+        err = np.mean(err)
+    errors.append(err)
+plt.loglog(dtSizes, errors, 'o-', label="SDC")
 
 
 # non-SDC runs
@@ -79,10 +80,9 @@ errors = []
 for i, dt in enumerate(dtSizes):
     dirName = f"{baseDir}/run_nosdc_dt{dt:1.1e}"
     os.makedirs(dirName, exist_ok=True)
-    if True:
-        print(f" -- running non-SDC simulation with dt={dt:1.1e} in {dirName}")
-        runSim(dirName, Rayleigh, resFactor, baseDt=dt, useSDC=False,
-               tEnd=tEnd, dtWrite=tEnd, initFields=initFields)
+    print(f" -- running non-SDC simulation with dt={dt:1.1e} in {dirName}")
+    runSim(dirName, Rayleigh, resFactor, baseDt=dt, useSDC=False,
+           tEnd=tEnd, dtWrite=tEnd, initFields=initFields)
     numFields = OutputFiles(dirName).file(0)['tasks']
     diff = numFields[var][-1] - refFields[var][-1]
     err = np.linalg.norm(
@@ -91,5 +91,6 @@ for i, dt in enumerate(dtSizes):
         err = np.mean(err)
     errors.append(err)
 
-plt.loglog(dtSizes, errors, label="RK443")
+plt.ylim(ymax=1e-2)
+plt.loglog(dtSizes, errors, 'o-', label="RK443")
 plt.legend()
