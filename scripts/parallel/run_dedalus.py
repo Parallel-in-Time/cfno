f0bfaacffee4951cf7aa42bc4e69a5131bbf5597
Thibaut Lunet <thibaut.lunet@tuhh.de>
2025-01-03 19:10:40 +0100

    TL: added more options to run_dedalus.py

--- scripts/parallel/run_dedalus.py
+++ scripts/parallel/run_dedalus.py
@@ -10,15 +10,24 @@ parser.add_argument(
     "--useSDC", action="store_true", help="use SDC time integrator")
 parser.add_argument(
     "--timeParallel", action="store_true", help="use SDC time-parallel integrator")
+parser.add_argument(
+    "--groupTime", action="store_true", help="group time-parallel processes")
 args = parser.parse_args()
 
 tEnd = 1
 useSDC = args.useSDC or args.timeParallel
 timeParallel = args.timeParallel
+groupTime = args.groupTime
 
 dirID = f"{MPI_SIZE:03d}"
-if useSDC: dirID += "_sdc"
-if timeParallel: dirID += "_tPar"
+if useSDC: 
+    dirID += "_sdc"
+else:
+    dirID += "_rk3"
+if timeParallel:
+    dirID += "_tPar"
+    if groupTime:
+        dirID += "G"
 
 SpectralDeferredCorrectionIMEX.setParameters(
     nNodes=4, implSweep="MIN-SR-FLEX", explSweep="PIC", initSweep="COPY"
@@ -27,7 +36,7 @@ SpectralDeferredCorrectionIMEX.setParameters(
 infos, solver = runSim(
     f"scaling_{dirID}",
     tEnd=tEnd, dtWrite=2*tEnd, writeSpaceDistr=True, logEvery=10000,
-    useSDC=useSDC, timeParallel=timeParallel)
+    useSDC=useSDC, timeParallel=timeParallel, groupTimeProcs=groupTime)
 if MPI_RANK == 0:
     with open(f"infos_{dirID}.txt", "w") as f:
         f.write(str(infos)+"\n")
