09efa8b21eeccbc9287e5f8e47f9d440b591b32d
Thibaut Lunet <thibaut.lunet@tuhh.de>
2025-10-10 14:05:43 +0200

    TL: started interpolation experiment

--- scripts/99_interpolationExperiment.py
+++ scripts/99_interpolationExperiment.py
@@ -5,8 +5,14 @@ Created on Fri Oct 10 10:47:07 2025
 
 @author: cpf5546
 """
+import numpy as np
+
+from qmat.lagrange import LagrangeApproximation
+from qmat.nodes import NodesGenerator
+
 from cfno.data.preprocessing import HDF5Dataset
 from cfno.training.pySDC import FourierNeuralOp
+from cfno.simulation.post import contourPlot
 
 dataFile = "datasets/dataset_512x128_Ra1e7_dt1e-3_update.h5"
 modelFile = "models/model_run24_dt1e-3.pt"
@@ -14,3 +20,57 @@ modelFile = "models/model_run24_dt1e-3.pt"
 
 dataset = HDF5Dataset(dataFile)
 model = FourierNeuralOp(checkpoint=modelFile)
+
+
+iSample = 20
+
+u0 = dataset.inputs[iSample]
+uRef = dataset.outputs[iSample]
+
+uRef /= dataset.outScaling
+uRef += u0
+
+uPred = model(u0)
+
+xGrid = dataset.infos["xGrid"][:]
+yGrid = dataset.infos["yGrid"][:]
+
+xGridCoarse = xGrid[::2]
+yGridCoarse = NodesGenerator(nodeType="CHEBY-1", quadType="GAUSS").getNodes(yGrid.size//2)
+yGridCoarse += 1
+yGridCoarse /= 2
+R = LagrangeApproximation(
+    yGrid, weightComputation="STABLE").getInterpolationMatrix(yGridCoarse)
+P = LagrangeApproximation(
+    yGridCoarse, weightComputation="STABLE").getInterpolationMatrix(yGrid)
+
+
+nVar, nX, nZ = u0.shape
+# Restriction to coarse grid
+u0Coarse = (R @ u0[:, ::2, :].reshape(-1, nZ).T).T.reshape(nVar, nX//2, nZ//2)
+uRefCoarse = (R @ uRef[:, ::2, :].reshape(-1, nZ).T).T.reshape(nVar, nX//2, nZ//2)
+# Model evaluation
+uPredCoarse = model(u0Coarse)
+# Interpolation to fine grid
+uPred2 = np.fft.irfft(np.fft.rfft(uPredCoarse, axis=1), n=nX, axis=1)
+uPred2 = (P @ uPred2.reshape(-1, nZ//2).T).T.reshape(nVar, nX, nZ)
+
+uI = u0[2].T
+uM = uPred[2].T
+uM2 = uPred2[2].T
+uR = uRef[2].T
+
+contourPlot(
+    uI, xGrid, yGrid, title="Initial solution",
+    refField=uR, refTitle="Dedalus solution reference",
+    closeFig=False)
+
+contourPlot(
+    uM-uI, xGrid, yGrid, title="Model update using fine initial field",
+    refField=uR-uI, refTitle="Dedalus update reference",
+    refScales=True, closeFig=False)
+
+contourPlot(
+    uM2-uI, xGrid, yGrid, title="Model update using coarse initial field",
+    refField=uR-uI, refTitle="Dedalus update reference",
+    refScales=True, closeFig=False)
--- tests/test_interpolation.py
+++ tests/test_interpolation.py
@@ -5,7 +5,6 @@ Created on Tue Apr 15 17:28:43 2025
 
 @author: telu
 """
-import gc
 import timeit
 import numpy as np
 import matplotlib.pyplot as plt
@@ -33,6 +32,8 @@ def setup(nX):
     uI1 = np.einsum('ij,tvxyj->tvxyi', P, uBase)
     uI2 = (P @ uBase.reshape(-1, nX).T).T.reshape(nFields, dim, *[nX]*dim)
     assert np.allclose(uI1, uI2)
+    uI3 = (P @ uBase[..., None])[..., 0]
+    assert np.allclose(uI1, uI3)
 
     return uBase, P
 
@@ -48,7 +49,8 @@ def timeCall(cmd):
 
 tEinsum = []
 tMatmul = []
-nXValues = [32, 64, 128]
+tMatmul2 = []
+nXValues = [32, 64, 128, 256]
 for nX in nXValues:
     uBase, P = setup(nX)
     t1 = timeCall(
@@ -57,7 +59,11 @@ for nX in nXValues:
     t2 = timeCall(
         "uI2 = (P @ uBase.reshape(-1, nX).T).T.reshape(nFields, dim, *[nX]*dim)")
     tMatmul.append(t2/(nFields*nX**dim))
+    t3 = timeCall(
+        "uI2 = (P @ uBase[..., None])[..., 0]")
+    tMatmul2.append(t3/(nFields*nX**dim))
 
 plt.loglog(nXValues, tEinsum, label=f"Einsum, nFields={nFields}")
 plt.loglog(nXValues, tMatmul, label=f"Matmul, nFields={nFields}")
+plt.loglog(nXValues, tMatmul2, label=f"Matmul2, nFields={nFields}")
 plt.legend()
